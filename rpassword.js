e91344aecfae3b41c12480054c95b29f0db80e0e
unitejohndalev <91070572+unitejohndalev@users.noreply.github.com>
2022-08-25 21:51:10 +0800

    passrandom

---
+++ rpassword.js
@@ -0,0 +1,233 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+"N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e",
+"f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
+"x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!",
+"@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",",
+"|",":",";","<",">",".","?",
+"/"];
+
+
+let geN1 = document.querySelector(".gen1")
+let geN2 = document.querySelector(".gen2")
+let genPass = document.querySelector(".genpass")
+
+genPass.addEventListener("click", function(){
+  let genPass1 = Math.floor(Math.random() * characters.length)
+  let genPass2 = Math.floor(Math.random() * characters.length)
+  let genPass3 = Math.floor(Math.random() * characters.length)
+  let genPass4 = Math.floor(Math.random() * characters.length)
+  let genPass5 = Math.floor(Math.random() * characters.length)
+  let genPass6 = Math.floor(Math.random() * characters.length)
+  let genPass7 = Math.floor(Math.random() * characters.length)
+  let genPass8 = Math.floor(Math.random() * characters.length)
+  let genPass9 = Math.floor(Math.random() * characters.length)
+  let genPass10 = Math.floor(Math.random() * characters.length)
+  let genPass11 = Math.floor(Math.random() * characters.length)
+  let genPass12 = Math.floor(Math.random() * characters.length)
+  let genPass13 = Math.floor(Math.random() * characters.length)
+  let genPass14 = Math.floor(Math.random() * characters.length)
+  let genPass15 = Math.floor(Math.random() * characters.length)
+
+
+
+  geN1.textContent =
+  characters[genPass1] + characters[genPass2] +
+  characters[genPass3] + characters[genPass4] + 
+  characters[genPass5] + characters[genPass6] +
+  characters[genPass7] + characters[genPass8] + 
+  characters[genPass9] + characters[genPass10] +
+  characters[genPass11] + characters[genPass12] + 
+  characters[genPass13] + characters[genPass14] +
+  characters[genPass15] 
+ 
+
+  let genPass112 = Math.floor(Math.random() * characters.length)
+  let genPass22 = Math.floor(Math.random() * characters.length)
+  let genPass33 = Math.floor(Math.random() * characters.length)
+  let genPass44 = Math.floor(Math.random() * characters.length)
+  let genPass55 = Math.floor(Math.random() * characters.length)
+  let genPass66 = Math.floor(Math.random() * characters.length)
+  let genPass77 = Math.floor(Math.random() * characters.length)
+  let genPass88 = Math.floor(Math.random() * characters.length)
+  let genPass99 = Math.floor(Math.random() * characters.length)
+  let genPass100 = Math.floor(Math.random() * characters.length)
+  let genPass111 = Math.floor(Math.random() * characters.length)
+  let genPass122 = Math.floor(Math.random() * characters.length)
+  let genPass133 = Math.floor(Math.random() * characters.length)
+  let genPass144 = Math.floor(Math.random() * characters.length)
+  let genPass155 = Math.floor(Math.random() * characters.length)
+
+  geN2.textContent =
+  characters[genPass112] + characters[genPass22] +
+  characters[genPass33] + characters[genPass44] + 
+  characters[genPass55] + characters[genPass66] +
+  characters[genPass77] + characters[genPass88] + 
+  characters[genPass99] + characters[genPass100] +
+  characters[genPass111] + characters[genPass122] + 
+  characters[genPass133] + characters[genPass144] +
+  characters[genPass155]
+
+})
+
+
+
+
+  const number7 = document.querySelector(".number7")
+  const dropbtn = document.querySelector(".dropbtn") 
+
+
+//   for 7 length
+
+number7.addEventListener ("click", function(){
+    let numlength = 7
+     dropbtn.textContent = "Length: " + "07"
+    genPass.addEventListener("click", function(){
+   
+  let genPass1 = Math.floor(Math.random() * characters.length)
+  let genPass2 = Math.floor(Math.random() * characters.length)
+  let genPass3 = Math.floor(Math.random() * characters.length)
+  let genPass4 = Math.floor(Math.random() * characters.length)
+  let genPass5 = Math.floor(Math.random() * characters.length)
+  let genPass6 = Math.floor(Math.random() * characters.length)
+  let genPass7 = Math.floor(Math.random() * characters.length)
+
+  
+  geN1.textContent =
+  characters[genPass1] + characters[genPass2] +
+  characters[genPass3] + characters[genPass4] + 
+  characters[genPass5] + characters[genPass6] +
+  characters[genPass7]
+    
+
+  let genPass112 = Math.floor(Math.random() * characters.length)
+  let genPass22 = Math.floor(Math.random() * characters.length)
+  let genPass33 = Math.floor(Math.random() * characters.length)
+  let genPass44 = Math.floor(Math.random() * characters.length)
+  let genPass55 = Math.floor(Math.random() * characters.length)
+  let genPass66 = Math.floor(Math.random() * characters.length)
+  let genPass77 = Math.floor(Math.random() * characters.length)
+
+
+  geN2.textContent =
+  characters[genPass112] + characters[genPass22] +
+  characters[genPass33] + characters[genPass44] + 
+  characters[genPass55] + characters[genPass66] +
+  characters[genPass77]
+}) 
+}) 
+
+const number12 = document.querySelector(".number12")
+// for 12 length
+number12.addEventListener ("click", function(){
+   dropbtn.textContent = "Length: " + "12"
+    genPass.addEventListener("click", function(){
+   
+  let genPass1 = Math.floor(Math.random() * characters.length)
+  let genPass2 = Math.floor(Math.random() * characters.length)
+  let genPass3 = Math.floor(Math.random() * characters.length)
+  let genPass4 = Math.floor(Math.random() * characters.length)
+  let genPass5 = Math.floor(Math.random() * characters.length)
+  let genPass6 = Math.floor(Math.random() * characters.length)
+  let genPass7 = Math.floor(Math.random() * characters.length)
+  let genPass8 = Math.floor(Math.random() * characters.length)
+  let genPass9 = Math.floor(Math.random() * characters.length)
+  let genPass10 = Math.floor(Math.random() * characters.length)
+  let genPass11 = Math.floor(Math.random() * characters.length)
+  let genPass12 = Math.floor(Math.random() * characters.length)
+
+  
+ geN1.textContent =
+  characters[genPass1] + characters[genPass2] +
+  characters[genPass3] + characters[genPass4] + 
+  characters[genPass5] + characters[genPass6] +
+  characters[genPass7] + characters[genPass8] + 
+  characters[genPass9] + characters[genPass10] +
+  characters[genPass11] + characters[genPass12]
+    
+
+  let genPass112 = Math.floor(Math.random() * characters.length)
+  let genPass22 = Math.floor(Math.random() * characters.length)
+  let genPass33 = Math.floor(Math.random() * characters.length)
+  let genPass44 = Math.floor(Math.random() * characters.length)
+  let genPass55 = Math.floor(Math.random() * characters.length)
+  let genPass66 = Math.floor(Math.random() * characters.length)
+  let genPass77 = Math.floor(Math.random() * characters.length)
+  let genPass88 = Math.floor(Math.random() * characters.length)
+  let genPass99 = Math.floor(Math.random() * characters.length)
+  let genPass100 = Math.floor(Math.random() * characters.length)
+  let genPass111 = Math.floor(Math.random() * characters.length)
+  let genPass122 = Math.floor(Math.random() * characters.length)
+
+
+  geN2.textContent =
+  characters[genPass112] + characters[genPass22] +
+  characters[genPass33] + characters[genPass44] + 
+  characters[genPass55] + characters[genPass66] +
+  characters[genPass77] + characters[genPass88] + 
+  characters[genPass99] + characters[genPass100] +
+  characters[genPass111] + characters[genPass122] 
+
+}) 
+}) 
+
+// for 14 length
+const number14 = document.querySelector(".number14")
+
+number14.addEventListener ("click", function(){
+      dropbtn.textContent = "Length: " + "14"
+    genPass.addEventListener("click", function(){
+   
+  let genPass1 = Math.floor(Math.random() * characters.length)
+  let genPass2 = Math.floor(Math.random() * characters.length)
+  let genPass3 = Math.floor(Math.random() * characters.length)
+  let genPass4 = Math.floor(Math.random() * characters.length)
+  let genPass5 = Math.floor(Math.random() * characters.length)
+  let genPass6 = Math.floor(Math.random() * characters.length)
+  let genPass7 = Math.floor(Math.random() * characters.length)
+  let genPass8 = Math.floor(Math.random() * characters.length)
+  let genPass9 = Math.floor(Math.random() * characters.length)
+  let genPass10 = Math.floor(Math.random() * characters.length)
+  let genPass11 = Math.floor(Math.random() * characters.length)
+  let genPass12 = Math.floor(Math.random() * characters.length)
+  let genPass13 = Math.floor(Math.random() * characters.length)
+  let genPass14 = Math.floor(Math.random() * characters.length)
+
+  
+ geN1.textContent =
+  characters[genPass1] + characters[genPass2] +
+  characters[genPass3] + characters[genPass4] + 
+  characters[genPass5] + characters[genPass6] +
+  characters[genPass7] + characters[genPass8] + 
+  characters[genPass9] + characters[genPass10] +
+  characters[genPass11] + characters[genPass12] + 
+  characters[genPass13] + characters[genPass14]
+
+
+  let genPass112 = Math.floor(Math.random() * characters.length)
+  let genPass22 = Math.floor(Math.random() * characters.length)
+  let genPass33 = Math.floor(Math.random() * characters.length)
+  let genPass44 = Math.floor(Math.random() * characters.length)
+  let genPass55 = Math.floor(Math.random() * characters.length)
+  let genPass66 = Math.floor(Math.random() * characters.length)
+  let genPass77 = Math.floor(Math.random() * characters.length)
+  let genPass88 = Math.floor(Math.random() * characters.length)
+  let genPass99 = Math.floor(Math.random() * characters.length)
+  let genPass100 = Math.floor(Math.random() * characters.length)
+  let genPass111 = Math.floor(Math.random() * characters.length)
+  let genPass122 = Math.floor(Math.random() * characters.length)
+  let genPass133 = Math.floor(Math.random() * characters.length)
+  let genPass144 = Math.floor(Math.random() * characters.length)
+ 
+
+  geN2.textContent =
+  characters[genPass112] + characters[genPass22] +
+  characters[genPass33] + characters[genPass44] + 
+  characters[genPass55] + characters[genPass66] +
+  characters[genPass77] + characters[genPass88] + 
+  characters[genPass99] + characters[genPass100] +
+  characters[genPass111] + characters[genPass122] + 
+  characters[genPass133] + characters[genPass144] 
+    
+
+}) 
+}) 
